168745cb7f599332c3cfbc92d990e9a7b2c82bcd
Baek Gi Chan <laphayen@gmail.com>
2024-02-06 22:40:53 +0900

    Update JUnitTest

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("JUnitTest 클래스 테스트")
 public class JUnitTest {
-    @DisplayName("덧셈 테스트")
-    @Test
-    public void addJunitTest () {
-        MyMath math = new MyMath();
-        int result = math.add(3, 5);
-
-        // 오버로딩된 assertEquals 메소드 사용
-        Assertions.assertEquals(8, result);
-
-        // 문자열 비교
-        Assertions.assertEquals("Hello", "Hello");
-
-        // 부동소수점 비교 (오차범위 0.0000000000001)
-        Assertions.assertEquals(3.14, 3.14, 0.0000000000001);
-    }
+//    @DisplayName("덧셈 테스트")
+//    @Test
+//    public void addJunitTest () {
+//        MyMath math = new MyMath();
+//        int result = math.add(3, 5);
+//
+//        // 오버로딩된 assertEquals 메소드 사용
+//        Assertions.assertEquals(8, result);
+//
+//        // 문자열 비교
+//        Assertions.assertEquals("Hello", "Hello");
+//
+//        // 부동소수점 비교 (오차범위 0.0000000000001)
+//        Assertions.assertEquals(3.14, 3.14, 0.0000000000001);
+//    }
 }
