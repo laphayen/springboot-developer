6ca0ee7655793a60663c7236b776df7286da7a57
Baek Gi Chan <laphayen@gmail.com>
2024-02-18 04:48:59 +0900

    Upload MemberService.java

---
+++ src/main/java/me/laphayen/springbootdeveloper/MemberService.java
@@ -0,0 +1,25 @@
+package me.laphayen.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void test() {
+        // 생성 Create
+        memberRepository.save(new Member(1L, "A"));
+
+        // 조회 Read
+        Optional<Member> member = memberRepository.findById(1L);
+        List<Member> allMemebers = memberRepository.findAll();
+
+        // 삭제 Delete
+        memberRepository.deleteById(1L);
+    }
+}
