4b45897f79fa0f84cc9dee3dd076fc839e51696e
Baek Gi Chan <laphayen@gmail.com>
2024-02-24 10:58:18 +0900

    Update test code

--- board/src/test/java/me/laphayen/SpringBootDeveloperApplication/controller/BlogApiControllerTest.java
+++ board/src/test/java/me/laphayen/SpringBootDeveloperApplication/controller/BlogApiControllerTest.java
@@ -1,18 +1,29 @@
 package me.laphayen.SpringBootDeveloperApplication.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.laphayen.SpringBootDeveloperApplication.domain.Article;
+import me.laphayen.SpringBootDeveloperApplication.dto.AddArticleRequest;
 import me.laphayen.SpringBootDeveloperApplication.repository.BlogRepository;
 import org.apache.catalina.loader.WebappClassLoader;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,5 +45,32 @@ class BlogApiControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
         blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 블로그 글 추가에 성공")
+    @Test
+    public void addArticl() throws Exception {
+        // given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        // 객체 JSON 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        // when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
 }
